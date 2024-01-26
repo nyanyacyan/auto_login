a83ba397175dc8b76379f3ce60a3beaab1d5ba37
nyanyacyan <nyanyacyan@gmail.com>
2024-01-27 07:10:52 +0900

    token部分を修正。

--- lineNotify.py
+++ lineNotify.py
@@ -1,10 +1,13 @@
+import os
 import requests
+from dotenv import load_dotenv
 
 class LineNotify:
     def __init__(self):
+        load_dotenv()
         # 作成したトークンを挿入
         # LINE通知したい人を選定してトークン作成=> ここに貼り付ける
-        self.line_notify_token = '2YDEXfVx0mgOrCAY4HOa5wRPRyKArCvY2oBAkfMyKjk'
+        self.line_notify_token = os.getenv('LINE_NOTIFY_TOKEN')
 
     def line_notify(self, notification_message):
         """
